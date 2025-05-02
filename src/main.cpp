0c3b8e7f26a33047e00522d1dc01a01920c524f0
BrunoPaoloFigueroa <bruno.figueroa@usil.pe>
2025-05-02 04:34:34 -0500

    V2

--- src/main.cpp
+++ src/main.cpp
@@ -2,3 +2,18 @@
 #include <avr/interrupt.h>
 #define F_CPU 16000000UL
 #include <util/delay.h>
+
+char pos=0;
+
+void config_PCI(void){
+
+      PCICR |= 0x04;              
+      PCMSK2 |= 0x0E;             
+
+      DDRD &= ~(0x0E);            
+      PORTD |= 0x0E;              
+
+      DDRD |= 0xF0;  
+
+
+}
