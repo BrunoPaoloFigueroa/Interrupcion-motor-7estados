ca160ca83a60a57ca23d95d052888b4388e65b1c
BrunoPaoloFigueroa <bruno.figueroa@usil.pe>
2025-05-02 04:52:23 -0500

    v4

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 #include <avr/interrupt.h>
 #define F_CPU 16000000UL
 #include <util/delay.h>
-
+char comando=0;
 char pos=0;
 
 void config_PCI(void){
@@ -19,101 +19,133 @@ void config_PCI(void){
 
 
 ISR(PCINT2_vect) {
-  char a=PIND&0X0E;
+  char entrada=PIND&0X0E;
   _delay_ms(100);
-      switch (a){
+  switch (entrada) {
+    case 0x02:              
+        comando = 0x02;
+        break;
+    case 0x04:             
+        comando = 0x04;
+        break;
+    case 0x06:          
+        comando = 0x06;
+        break;
+    case 0x08:           
+        comando = 0x08;
+        break;
+    case 0x0A:             
+        comando = 0x0A;
+        break;
+    case 0x0C:              
+        comando = 0x0C;
+        break;
+    case 0x0E:              
+        comando = 0x0E;
+        break;
+    default:
+        break;
+}
+}
+
+void tarea(char t){
+
+  switch (t){
 
       
 
-          case 0x02:
+    case 0x02:
 
-          //PARO
-          PORTD&=~(0x10);   //Motor A
-          PORTD&=~(0x20);
+    //PARO
+    PORTD&=~(0x10);   //Motor A
+    PORTD&=~(0x20);
 
-          PORTD&=~(0x40);   //Motor B
-          PORTD&=~(0x80);
+    PORTD&=~(0x40);   //Motor B
+    PORTD&=~(0x80);
 
 
-          break;
+    break;
 
-          case 0x04:
+    case 0x04:
 
-          //Avance
-          PORTD|=0x10;   //Motor A
-          PORTD&=~(0x20);
+    //Avance
+    PORTD|=0x10;   //Motor A
+    PORTD&=~(0x20);
 
-          PORTD|=0x40;   //Motor B
-          PORTD&=~(0x80);
-       
+    PORTD|=0x40;   //Motor B
+    PORTD&=~(0x80);
+ 
 
-          break;
+    break;
 
-          case 0x06:
+    case 0x06:
 
-          //Retro
-          PORTD&=~(0x10);   //Motor A
-          PORTD|=(0x20);
-      
-         
-          PORTD&=~(0x40);   //Motor B
-          PORTD|=(0x80);
+    //Retro
+    PORTD&=~(0x10);   //Motor A
+    PORTD|=(0x20);
 
-          break;
+   
+    PORTD&=~(0x40);   //Motor B
+    PORTD|=(0x80);
 
-          case 0x08:
+    break;
 
-          //Derecha Larga
-          PORTD&=~(0x10);   //Motor A
-          PORTD&=~(0x20);
+    case 0x08:
 
-          PORTD|=0x40;   //Motor B
-          PORTD&=~(0x80);
+    //Derecha Larga
+    PORTD&=~(0x10);   //Motor A
+    PORTD&=~(0x20);
 
-          break;
+    PORTD|=0x40;   //Motor B
+    PORTD&=~(0x80);
 
-          case 0x0A:
+    break;
 
-          //Derecha Corta
-          PORTD&=~(0x10);   //Motor A
-          PORTD|=(0x20);
+    case 0x0A:
 
-          PORTD|=0x40;   //Motor B
-          PORTD&=~(0x80);
+    //Derecha Corta
+    PORTD&=~(0x10);   //Motor A
+    PORTD|=(0x20);
 
-          break;
+    PORTD|=0x40;   //Motor B
+    PORTD&=~(0x80);
 
-          case 0x0C:
+    break;
 
-          //Izquierda Larga
-          PORTD|=(0x10);   //Motor A
-          PORTD&=~(0x20);
+    case 0x0C:
 
-          PORTD&=~(0x40);   //Motor B
-          PORTD&=~(0x80);
+    //Izquierda Larga
+    PORTD|=(0x10);   //Motor A
+    PORTD&=~(0x20);
 
-          break;
+    PORTD&=~(0x40);   //Motor B
+    PORTD&=~(0x80);
 
-          case 0x0E:
+    break;
 
-           //Izquierda Corta
-           PORTD|=(0x10);   //Motor A
-           PORTD&=~(0x20);
+    case 0x0E:
 
-           PORTD&=~(0x40);   //Motor B
-           PORTD|=(0x80);
+     //Izquierda Corta
+     PORTD|=(0x10);   //Motor A
+     PORTD&=~(0x20);
 
-           break;      
+     PORTD&=~(0x40);   //Motor B
+     PORTD|=(0x80);
 
-      }
+     break;      
+
+}
+
 }
 
 int main(void){
     DDRD |= 0xF0;  
+    PORTD &= ~(0xF0);
     config_PCI();
     sei();    
     while(1){
-
+      tarea(comando);
+      _delay_ms(500);
     }
   
 }
